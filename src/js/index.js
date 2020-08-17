8b7409e67f3e5e2d00d4e0162a32b2845fcc1fca
nikita <nikitka.kuznetsov.1994@mail.ru>
2020-08-17 21:34:19 +0300

    блоки face и problems готовы

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,45 @@
 
-// $(document).ready(function() {
-//   console.log(1)
-// });
+$(document).ready(function() {
+    $('.form__field').focus( function() {
+        $(this).addClass('form__field_active')
+    }).blur(function () {
+        if ( $(this).val() == "") {
+            $(this).removeClass('form__field_active')
+        }
+    })
+
+    Start();
+    function Start() {
+        var m = 22,
+            s = 0;
+
+        if (m <= 9) {
+            m = "0" + m;
+        };
+
+        var timerId = setTimeout(function tick() {
+            if (s != 0) {
+                s = s - 1;
+
+                if (s <= 9) {
+                    s = "0" + s;
+                }
+            } else {
+                if (m != 0) {
+                    m = m - 1;
+                    s = 59;
+
+                    if (m <= 9) {
+                        m = "0" + m;
+                    }
+                } else {
+                    return
+                }
+            }
+
+            $('.form__time-count:nth-of-type(2) span:first-child').text(m);
+            $('.form__time-count:last-child span:first-child').text(s);
+            timerId = setTimeout(tick, 1000);
+        }, 1000);
+    }
+});
